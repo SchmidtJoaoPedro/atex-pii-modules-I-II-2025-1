86d59f2079103a0489e7d58b81685106d396efb2
RafaelEngel10 <rafaelengelserafim09@gmail.com>
2025-12-17 11:35:26 -0300

    atualização nos caminhos relativos

--- javascript/jokescript.js
+++ javascript/jokescript.js
@@ -1,16 +1,16 @@
 let alternar = true;
 
 function trocarImagem() {
-    document.getElementById("alemão").src = alternar ? "../navcode/images/partyimages/partyimages-joke/alemanha.png" : "../navcode/images/partyimages/Alemão.jpeg";
-    document.getElementById("francês").src = alternar ? "../navcode/images/partyimages/partyimages-joke/frança.png" : "../navcode/images/partyimages/Francês.jpeg";
-    document.getElementById("italiano").src = alternar ? "../navcode/images/partyimages/partyimages-joke/italia.png" : "../navcode/images/partyimages/Italiano.jpeg";
-    document.getElementById("russo").src = alternar ? "../navcode/images/partyimages/partyimages-joke/russia.png" : "../navcode/images/partyimages/Russo.jpeg";
+    document.getElementById("alemão").src = alternar ? "./images/partyimages/partyimages-joke/alemanha.png" : "./images/partyimages/Alemão.jpeg";
+    document.getElementById("francês").src = alternar ? "./images/partyimages/partyimages-joke/frança.png" : "./images/partyimages/Francês.jpeg";
+    document.getElementById("italiano").src = alternar ? "./images/partyimages/partyimages-joke/italia.png" : "./images/partyimages/Italiano.jpeg";
+    document.getElementById("russo").src = alternar ? "./images/partyimages/partyimages-joke/russia.png" : "./images/partyimages/Russo.jpeg";
 
-    document.getElementById("suiço").src = alternar ? "../navcode/images/partyimages/partyimages-joke/lituano.png" : "../navcode/images/partyimages/Suiço.jpeg";
+    document.getElementById("suiço").src = alternar ? "./images/partyimages/partyimages-joke/lituano.png" : "./images/partyimages/Suiço.jpeg";
     document.getElementById("suiço-lituano").textContent = alternar ? "Ryan Augusto Chagas" : "Thiago de Oliveira Becker Bayao";
 
-    document.getElementById("croata").src = alternar ? "../navcode/images/partyimages/partyimages-joke/croacia.png" : "../navcode/images/partyimages/Croata.jpeg";
-    document.getElementById("americano").src = alternar ? "../navcode/images/partyimages/partyimages-joke/america.png" : "../navcode/images/partyimages/Americano.jpeg";
+    document.getElementById("croata").src = alternar ? "./images/partyimages/partyimages-joke/croacia.png" : "./images/partyimages/Croata.jpeg";
+    document.getElementById("americano").src = alternar ? "./images/partyimages/partyimages-joke/america.png" : "./images/partyimages/Americano.jpeg";
 
     alternar = !alternar; 
 }
